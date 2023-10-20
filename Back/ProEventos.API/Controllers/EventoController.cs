8333ac152bafe3bf24ad560ef9ecc6782e05a299
Vitor Oliveira <vitor_cos12@yahoo.com.br>
2023-10-19 22:22:23 -0300

    creating database-DBContext and associations

--- Back/ProEventos.API/Controllers/EventoController.cs
+++ Back/ProEventos.API/Controllers/EventoController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ProEventos.API.Model;
+using ProEventos.API.Data;
 
 namespace ProEventos.API.Controllers;
 
@@ -8,41 +9,23 @@ namespace ProEventos.API.Controllers;
 public class EventoController : ControllerBase
 {
 
+    private readonly DataContext _context;
 
-    public IEnumerable<Evento> _evento = new Evento[]{
-        new Evento{
-           EventoId = 1,
-           Tema = "Angular 11 e .NET 6",
-           Local = "Belo Horizonte",
-           Lote = "1º Lote",
-           QtdPessoas = 250,
-           DataEvento = DateTime.Now.AddDays(2).ToString(),
-           ImagemURL = "foto.png"
-       },
-       new Evento{
-
-           EventoId = 2,
-           Tema = "Angular 11 e suas novidades",
-           Local = "São Paulo",
-           Lote = "2º Lote",
-           QtdPessoas = 350,
-           DataEvento = DateTime.Now.AddDays(3).ToString(),
-           ImagemURL = "foto.png"
-       }
-    };
-
-
+    public EventoController(DataContext context)
+    {
+        _context = context;
+    }
 
     [HttpGet]
     public IEnumerable<Evento> Get()
     {
-        return _evento;
+        return _context.Eventos;
     }
 
     [HttpGet("{id}")]
-    public IEnumerable<Evento> GetById(int id)
+    public Evento GetById(int id)
     {
-        return _evento.Where(e=>e.EventoId == id);
+        return _context.Eventos.FirstOrDefault(e=>e.EventoId == id);
     }
 
 
